51df603d55146e025f21e6395c06805f7242d74e
Michael Huang <huang.zha@husky.neu.edu>
2020-04-10 16:55:22 -0400

    Acivity page selection

--- web/src/cs3200project/components/dialog/exerciseEntriesDialog.js
+++ web/src/cs3200project/components/dialog/exerciseEntriesDialog.js
@@ -11,32 +11,39 @@ export default class ExerciseEntriesDialog extends React.Component {
             addMode: true,
             types: [],
             exerciseItems: [],
-            minutesSpentExercising: 0
+            minutesSpentExercising: 0,
+            selectedType: null,
+            selectedExerciseItem: null
         }
         this.exerciseService = new ExerciseService();
     }
 
     async componentWillMount() {
         const exerciseTypes = await this.exerciseService.getAllExerciseTypes();
+        await this.applyExerciseType(exerciseTypes.types[0]);
         this.setState({ types: exerciseTypes.types } );
     }
 
-    async applyExerciseType() {
-        if (!this.state.selectedType) {
-            this.setState({ failedToApplyExerciseType: true })
+    async applyExerciseType(type) {
+        if (!type) {
+            await this.setState({ failedToApplyExerciseType: true })
         }
         else {
+            await this.setState({selectedType: type });
             const exerciseItems = await this.exerciseService.getAllExerciseItemsByType(this.state.selectedType);
-            this.setState({ failedToApplyExerciseType: false, exerciseItems: exerciseItems.items });
-        };
+            await this.setState({
+                failedToApplyExerciseType: false,
+                exerciseItems: exerciseItems.items,
+                exerciseSelected: false });
+        }
     }
 
-    async applyExerciseItem() {
-        if (!this.state.selectedExerciseItem) {
+    async applyExerciseItem(exercise) {
+        if (!exercise) {
             this.setState({ failedToApplyExerciseItem: true });
         } else {
+            await this.setState({ selectedExerciseItem: exercise })
             const exerciseInfo = await this.exerciseService.getExerciseInfo(this.state.selectedExerciseItem.id);
-            console.log(exerciseInfo);
             this.setState({ exerciseInfo: exerciseInfo.entry }, () => {
                 this.setState({ failedToApplyExerciseItem: false, exerciseSelected: true })
             });
@@ -52,56 +59,46 @@ export default class ExerciseEntriesDialog extends React.Component {
             <div id="addMode">
                 <Typography className="h6-header" variant="h6">Select an exercise type:</Typography>
                 <div className="dialogRow">
-                    <Select 
-                        value={this.state.selectedType || this.state.types[0]}
-                        onChange={e => this.setState({ selectedType: e.target.value })}>
-                            {this.state.types.map(type => {
-                                return <MenuItem value={type}>{type}</MenuItem>
-                            })}
+                    <Select
+                        value={this.state.selectedType}
+                        onChange={e => this.applyExerciseType(e.target.value)}>
+                        {this.state.types.map(type => {
+                            return <MenuItem value={type}>{type}</MenuItem>
+                        })}
                     </Select>
-                    <Button
-                            className="button"
-                            onClick={() => this.applyExerciseType()}>
-                            Apply
-                    </Button>
                 </div>
-                
+
                 <Typography className="h6-header" variant="h6">Select an exercise:</Typography>
                 <div className="dialogRow">
-                    <Select 
-                        value={this.state.selectedExerciseItem || this.state.exerciseItems[0]}
-                        onChange={e => this.setState({ selectedExerciseItem: e.target.value })}>
-                            {this.state.exerciseItems.map(item => {
-                                return <MenuItem value={item}>{item.name}</MenuItem>
-                            })}
+                    <Select
+                        value={this.state.selectedExerciseItem}
+                        onChange={e => this.applyExerciseItem(e.target.value)}>
+                        {this.state.exerciseItems.map(item => {
+                            return <MenuItem value={item}>{item.name}</MenuItem>
+                        })}
                     </Select>
-                    <Button
-                            className="button"
-                            onClick={() => this.applyExerciseItem()}>
-                            Apply
-                    </Button>
                 </div>
 
                 {this.state.exerciseSelected &&
-                    <div className="exerciseInfoSection">
-                        <div className="dialogRow exerciseInfo">
-                        <text className="text">{this.state.exerciseInfo.at_130_lb} calories burned @ 130 lb</text>
-                        <text className="text">{this.state.exerciseInfo.at_155_lb} calories burned @ 155 lb</text>
-                        <text className="text">{this.state.exerciseInfo.at_180_lb} calories burned @ 180 lb</text>
-                        <text className="text">{this.state.exerciseInfo.at_205_lb} calories burned @ 205 lb</text>
-                        </div>
-
-                        <div className="dialogColumn">
-                            <Typography className="h6-header" variant="h6">
-                                Number of minutes spent exercising:
-                            </Typography>                     
-                        </div>
-                        <div className="dialogRow">
-                            <TextField
-                                variant="outlined"
-                                onChange={e => this.setState({ minutesSpentExercising: e.target.value })}/>
-                        </div>
+                <div className="exerciseInfoSection">
+                    <div className="dialogRow exerciseInfo">
+                        <text className="text">{this.state.exerciseInfo.at_130_lb} calories burned @ 130 lb per hour</text>
+                        <text className="text">{this.state.exerciseInfo.at_155_lb} calories burned @ 155 lb per hour</text>
+                        <text className="text">{this.state.exerciseInfo.at_180_lb} calories burned @ 180 lb per hour</text>
+                        <text className="text">{this.state.exerciseInfo.at_205_lb} calories burned @ 205 lb per hour</text>
                     </div>
+
+                    <div className="dialogColumn">
+                        <Typography className="h6-header" variant="h6">
+                            Number of minutes spent exercising:
+                        </Typography>
+                    </div>
+                    <div className="dialogRow">
+                        <TextField
+                            variant="outlined"
+                            onChange={e => this.setState({ minutesSpentExercising: e.target.value })}/>
+                    </div>
+                </div>
                 }
 
                 <Button
@@ -124,19 +121,19 @@ export default class ExerciseEntriesDialog extends React.Component {
                 <div className="dialogRow">
                     <Select
                         onChange={e => this.setState({ entryToEdit: e.target.value })}>
-                            {this.props.entries.map(entry => {
-                                console.log(entry);
-                                return <MenuItem value={entry}>{entry.name}</MenuItem>
-                            })}
+                        {this.props.entries.map(entry => {
+                            console.log(entry);
+                            return <MenuItem value={entry}>{entry.name}</MenuItem>
+                        })}
                     </Select>
                 </div>
                 { this.state.entryToEdit &&
-                    <div className="dialogRow">
-                        <div className="dialogColumn">
-                            <Typography className="h6-header" variant="h6">
-                                Number of minutes spent exercising:
-                            </Typography>
-                            <div className="dialogRow">
+                <div className="dialogRow">
+                    <div className="dialogColumn">
+                        <Typography className="h6-header" variant="h6">
+                            Number of minutes spent exercising:
+                        </Typography>
+                        <div className="dialogRow">
                             <TextField
                                 variant="outlined"
                                 onChange={e => {
@@ -144,9 +141,9 @@ export default class ExerciseEntriesDialog extends React.Component {
                                     updatedEntry.minutes_performed = e.target.value;
                                     this.setState({ entryToEdit: updatedEntry });
                                 }}/>
-                            </div>                      
                         </div>
                     </div>
+                </div>
                 }
                 <div className="buttonRow">
                     <Button
@@ -170,16 +167,16 @@ export default class ExerciseEntriesDialog extends React.Component {
         return (
             <Dialog className="formDialog" open={this.props.open}>
                 <DialogTitle>
-                    {this.props.title}    
+                    {this.props.title}
                 </DialogTitle>
-                {this.state.addMode 
+                {this.state.addMode
                     ? this.renderAddMode()
                     : this.renderEditMode()
                 }
                 {this.state.failedToApplyExerciseType &&
-                    <Typography variant="h5" color="error">Please select an exercise type.</Typography>}
+                <Typography variant="h5" color="error">Please select an exercise type.</Typography>}
                 {this.state.failedToApplyExerciseItem &&
-                    <Typography variant="h5" color="error">Please select an exercise.</Typography>}
+                <Typography variant="h5" color="error">Please select an exercise.</Typography>}
                 <div className="buttonRow">
                     <Button
                         className="button"
@@ -188,14 +185,14 @@ export default class ExerciseEntriesDialog extends React.Component {
                     </Button>
                 </div>
                 <Button
-                        className="button"
-                        onClick={() => this.setState({ 
-                            addMode: !this.state.addMode,
-                            entryToEdit: undefined,
-                            exerciseSelected: undefined,
-                            selectedType: undefined,
-                            selectedExerciseItem: undefined })}>
-                        {this.state.addMode ? "Enter Edit Mode" : "Enter Add Mode"}
+                    className="button"
+                    onClick={() => this.setState({
+                        addMode: !this.state.addMode,
+                        entryToEdit: undefined,
+                        exerciseSelected: undefined,
+                        selectedType: undefined,
+                        selectedExerciseItem: undefined })}>
+                    {this.state.addMode ? "Enter Edit Mode" : "Enter Add Mode"}
                 </Button>
             </Dialog>
         )
